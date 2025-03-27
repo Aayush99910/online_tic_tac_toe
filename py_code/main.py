c8bc4790a64f35dfe0a2ef51d7237646c2c6ed0b
Aayush <amaharjann01@gmail.com>
2025-03-26 20:29:48 -0700

    Removed build files from repository

--- py_code/main.py
+++ py_code/main.py
@@ -4,14 +4,14 @@ import gamelogic
 
 def main():
     # Initialize the board
-    game_board = gamelogic.initializeBoard()
+    gamelogic.initializeBoard()
 
     # Check if the board is being passed by reference
-    print("Initial board:", game_board)
+    print("Initial board:", gamelogic.getBoard())
 
     # Make a move and check if it modifies the original board
-    sucess = gamelogic.playerMakeMove(game_board, 0, 0, 'X')
-    print("Board after X move:", game_board)
+    sucess = gamelogic.playerMakeMove(0, 0, 'X')
+    print("Board after X move:", gamelogic.getBoard())
 
     # Make another move
     #sucess = mymodule.playerMakeMove(game_board, 1, 1, 'O')
--- src/GameLogic.cpp
+++ src/GameLogic.cpp
@@ -7,29 +7,36 @@
 
 namespace py = pybind11;
 
+// declaring a global board
+vector<vector<char>> globalBoard;
+
+// new function that will return the board to python 
+vector<vector<char>> getBoard(){
+  return globalBoard; // we are returning this board to python
+}
+
 // function to initialize the board
-vector<vector<char>> initializeBoard() {
-  return vector<vector<char>>(3, vector<char>(3, ' '));
+void initializeBoard() {
+  globalBoard = vector<vector<char>>(3, vector<char>(3, ' '));
 }
 
 // // functions to check for winning condition
-// passing in board as a reference to avoid copies
-bool isValidRow(vector<vector<char>> &board, int row, char target) {
+bool isValidRow(int row, char target) {
   // checking the same row
-  for (int j = 0; j < board.size(); j++) {
+  for (int j = 0; j < globalBoard.size(); j++) {
     // if anything in the same row is different than the target we return false
-    if (board[row][j] != target) return false;
+    if (globalBoard[row][j] != target) return false;
   }
 
   // if it is valid we return true
   return true;
 }
 
-bool isValidCol(vector<vector<char>> &board, int col, char target) {
+bool isValidCol(int col, char target) {
 	// checking the same col
-	for (int i = 0; i < board.size(); i++) {
+	for (int i = 0; i < globalBoard.size(); i++) {
     // if anything in the same col is different than the target we return false
-    if (board[i][col] != target) return false;
+    if (globalBoard[i][col] != target) return false;
 	}
 
   // if everything was the same then we return true
@@ -38,36 +45,36 @@ bool isValidCol(vector<vector<char>> &board, int col, char target) {
 
 // function to check for valid diagonals
 // if any diagonal is all same character then this function will return true 
-bool isValidDiagonal(vector<vector<char>> &board, char target) {
-  int size = board.size(); // getting the size of the board 
+bool isValidDiagonal(char target) {
+  int size = globalBoard.size(); // getting the size of the board 
   bool leftDiagonal;
   bool rightDiagonal;
 
   // loop to go through the board 
   for (int i = 0; i < size; i++) {
-    if (board[i][i] != target) leftDiagonal = false; 
-    if (board[i][size - i - 1] != target) rightDiagonal = false; 
+    if (globalBoard[i][i] != target) leftDiagonal = false; 
+    if (globalBoard[i][size - i - 1] != target) rightDiagonal = false; 
   }
 
   return leftDiagonal || rightDiagonal;
 }
 
 // this function will check if the player has won after their turn in the board
-bool hasWon(vector<vector<char>> &board){
+bool hasWon(){
 	// we are going to iterate over the matrix and check for each [i][j] in the matrix
-	for (int i = 0; i < board.size(); i++) {
-		for (int j = 0; j < board.size();  j++) {
+	for (int i = 0; i < globalBoard.size(); i++) {
+		for (int j = 0; j < globalBoard.size();  j++) {
       // we are checking if the position is ' ' 
       // if it is then we don't have to check for it because its not any character but empty cell
-      if (board[i][j] != ' ') {
+      if (globalBoard[i][j] != ' ') {
         // if the row or col is valid then we return true
-        if ((isValidRow(board, j, board[i][j])) || isValidCol(board, i, board[i][j])) {
+        if ((isValidRow(j, globalBoard[i][j])) || isValidCol(i, globalBoard[i][j])) {
           return true;
         }
 
         // Check diagonals only for positions that are part of a diagonal
         if ((i == j) || (i + j == 2)) {
-          if (isValidDiagonal(board, board[i][j])) {
+          if (isValidDiagonal(globalBoard[i][j])) {
               return true;
           }
         }
@@ -83,17 +90,17 @@ bool hasWon(vector<vector<char>> &board){
 // if every position is filled in the board then it will check if haswon is true or not
 // if it is true then it will return false as some player has won 
 // else we are returning true if the board is filledup and if the haswon function is false
-bool isDraw(vector<vector<char>> &board) {
+bool isDraw() {
   int filled = 0;
-  int size = board.size();
+  int size = globalBoard.size();
 
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
-      if (board[i][j] != ' ') filled++;
+      if (globalBoard[i][j] != ' ') filled++;
     }
   }
 
-  if (filled == 9 && !(hasWon(board))) {
+  if (filled == 9 && !(hasWon())) {
       return true; 
   }
 
@@ -106,16 +113,16 @@ bool isDraw(vector<vector<char>> &board) {
 // might press on the same cell even when there is already some character present in that cell. 
 // If there is nothing in that cell and if we were able to put their move inside that cell, we will return 
 // true. If that is not the case, then we return false.
-bool playerMakeMove(vector<vector<char>> &board, int row, int col, char playerChoice) {
+bool playerMakeMove(int row, int col, char playerChoice) {
   // checking if the chosen cell is empty or not 
-  if (board[row][col] == ' ') {
+  if (globalBoard[row][col] == ' ') {
     cout << row << col << endl;
-    board[row][col] = playerChoice;
+    globalBoard[row][col] = playerChoice;
 
-    for (int i = 0; i < board.size(); i++){
-    	for (int j = 0; j < board.size(); j++) {
-		cout << board[i][j] << endl;
-	}
+    for (int i = 0; i < globalBoard.size(); i++){
+    	for (int j = 0; j < globalBoard.size(); j++) {
+        cout << globalBoard[i][j] << endl;
+      }
     }
     return true;
   }
@@ -129,4 +136,5 @@ PYBIND11_MODULE(gamelogic, m) {
     m.def("hasWon", &hasWon, "A function which checks if a player won or not");
     m.def("isDraw", &isDraw, "Check if it's a draw");
     m.def("playerMakeMove", &playerMakeMove, "Player makes a move");
-}
+    m.def("getBoard", &getBoard, "Returns the global board");
+};
