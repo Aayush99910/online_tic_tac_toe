9eb93b7e0fc9fbbf43b5dcd261a2ada622c72f6d
Aayush <amaharjann01@gmail.com>
2025-03-26 22:39:21 -0700

    1. Added a basic playabale game with 10 turns 2. There is no check for valid values

--- py_code/main.py
+++ py_code/main.py
@@ -1,23 +1,49 @@
+ # type: ignore
 import sys
 sys.path.append('../src/build')
-import gamelogic
+import gamelogic 
 
-def main():
-    # Initialize the board
+def display_board(board) -> None:
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if j == len(board[i]) - 1:
+                print(f" {board[i][j]} ")
+            else:
+                print(f" {board[i][j]} ", end="|")
+        
+        if i == len(board) - 1:
+            print()
+        else:
+            print("------------")
+    
+def game():
+    # Initialize the gameboard 
     gamelogic.initializeBoard()
+    player_symbol = 'X'
+    i = 0
+    display_board(gamelogic.getBoard())
+    while i < 10: 
+        print(f"Player {player_symbol} turn!")
+        x = int(input("Please provide the row you want to put your character at: "))
+        y = int(input("Please provide the column you want to put your character at: "))
+        
+        if gamelogic.playerMakeMove(x, y, player_symbol):
+            print("Move successful!")
+            if gamelogic.hasWon():
+                print(f"Player {player_symbol} won!")
+                display_board(gamelogic.getBoard())
+                break 
+            elif gamelogic.isDraw():
+                print(f"Game is tied!")
+                display_board(gamelogic.getBoard())
+                break 
+            else:
+                player_symbol = 'O' if player_symbol == 'X' else 'X'
+        else:
+            print("Please select a different place to insert as this place is already occupied")
 
-    # Check if the board is being passed by reference
-    print("Initial board:", gamelogic.getBoard())
-
-    # Make a move and check if it modifies the original board
-    sucess = gamelogic.playerMakeMove(0, 0, 'X')
-    print("Board after X move:", gamelogic.getBoard())
-
-    # Make another move
-    #sucess = mymodule.playerMakeMove(game_board, 1, 1, 'O')
-    #print("Board after O move:", game_board)
-
-
-main()
-
-
+        display_board(gamelogic.getBoard())
+        
+        i += 1
+    
+game()
