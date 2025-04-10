80a79cfd4a6b23db0762c95a382882e5443a108c
Aayush <amaharjann01@gmail.com>
2025-04-10 13:30:55 -0700

    1) Added event listener for DOMload whenever the DOM is loaded we are going to call the backend for home and we will display that in our frontend whenever someone comes to our home 2) Added sendMove function, handleWebSocket function. These functions are added in order to create a minimum websocket that will be used to show real time updates 3) There is no logic to store player_symbol for now. 4) Added try and catch to catch errors so that the application will not breakdown.

--- frontend/script.js
+++ frontend/script.js
@@ -3,76 +3,108 @@ const boardContainer = document.getElementById("game-board");
 const statusElement = document.getElementById("status");
 const startBtn = document.getElementById("start-btn");
 
+
 /* 
-currently the player is going to be X just like in chess default is white 
-here default is X
+    currently the player is going to be X just like in chess default is white 
+    here default is X
 */
 let currentPlayer = 'X';
+let ws;
+
+/* 
+  When the page loads we want to call the the '/' endpoint from the backend
+*/
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const response = await fetch("http://localhost:8000/");
+
+        // handling the messages here
+        const data = await response.json();
+        statusElement.textContent = data.message;
+    }
+    catch(err) {
+        console.log("Error", err);
+    }
+})
+
+
+function sendMove(i, j) {
+    const move = { 
+        row: i, 
+        col: j
+    };  
+    ws.send(JSON.stringify(move));
+}
+
+function handleWebSocket() {
+    const roomId = 1;  // Room ID should be passed dynamically (this is just a placeholder)
+    ws = new WebSocket(`ws://localhost:8000/ws/${roomId}`);
+
+    ws.onmessage = function(event) {
+        const data = JSON.parse(event.data);  // Parse the incoming data (JSON)
+        console.log(data)
+    };
+}
+
 
 /*
-render board function that takes in two argument first is the board itself 
-that we are going to render board variable is an array that will be turned 
-into html elements for user to interact with 
-hasWon argument is taken because we want to have the click event listener 
-only if the game has not being won if someone wins then we don't have to have 
-this event listener
+    render board function that takes in two argument first is the board itself 
+    that we are going to render board variable is an array that will be turned 
+    into html elements for user to interact with 
+    hasWon argument is taken because we want to have the click event listener 
+    only if the game has not being won if someone wins then we don't have to have 
+    this event listener
 */
 function renderBoard(board, hasWon) {
-  boardContainer.innerHTML = "";
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      const div = document.createElement("div");
-      div.className = "cell";
-      div.textContent = board[i][j];
+    boardContainer.innerHTML = "";
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            const div = document.createElement("div");
+            div.className = "cell";
+            div.textContent = board[i][j];
 
-      // adding the event listener only if the game hasnt been won or drawn
-      if (!hasWon) {
-        div.addEventListener("click", () => makeMove(i, j));
-      }
+            // adding the event listener only if the game hasnt been won or drawn
+            if (!hasWon) {
+                div.addEventListener("click", () => sendMove(i, j));
+            }
 
-      // adding to this to the parent div
-      boardContainer.appendChild(div);
+            // adding to this to the parent div
+            boardContainer.appendChild(div);
+        }
     }
-  }
 }
+
 
 /*
-this function is going to fetch '/move/{row}/{col}' and it also provides a 
-query parameters and the resulting board is rendered.
-for now there is a simple check but going to make this more functional
+    event listener for start button
+    calls a callback function where we are going to fetch '/start/' end point
+    and whatever that endpoint returns we are going to print it to the user
+    and then we are rendering the board for the user to interact with 
 */
-async function makeMove(row, col) {
-  const res = await fetch(`http://localhost:8000/move/${row}/${col}?player=${currentPlayer}`, {
-    method: "POST",
-  });
-  const data = await res.json();
-
-  // player has won 
-  if (data.win){  
-    renderBoard(data.board, true); 
-    statusElement.textContent = data.message;   
-    currentPlayer = 'X'; 
-  } else if (data.draw) {
-    renderBoard(data.board, true); 
-    statusElement.textContent = data.message;   
-    currentPlayer = 'X'; 
-  } else if (!data.valid) {
-    renderBoard(data.board, false);
-    statusElement.textContent = data.message;
-  } else {
-    statusElement.textContent = data.message;
-    renderBoard(data.board, false);
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-  }
-}
-
-// event listener for start button
-// calls a callback function where we are going to fetch '/start/' end point
-// and whatever that endpoint returns we are going to print it to the user
-// and then we are rendering the board for the user to interact with 
 startBtn.addEventListener("click", async () => {
-  const res = await fetch("http://localhost:8000/start", { method: "POST" });
-  const data = await res.json();
-  statusElement.textContent = data.message;
-  renderBoard(data.board);
+    try {
+        const res = await fetch("http://localhost:8000/start", { method: "POST" });
+        
+        // handling any errors given by backend here
+        if (!res.ok) {
+            const errorData = await res.json();
+            console.log("Error: " + errorData.detail);
+            return;
+        }
+
+        const data = await res.json();
+        statusElement.textContent = data.message;
+        renderBoard(data.board);
+
+        /* 
+            Testing websocket connection:
+            we should start websocket only after user has started the game 
+            after clicking the start button or else we should not start 
+            a websocket
+        */
+        handleWebSocket();
+    }
+    catch(err) {
+        console.log("Error", err);
+    }
 });
