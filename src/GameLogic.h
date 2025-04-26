63fc76b057532bb51d4fc0d4642c584125d771c0
Aayush <amaharjann01@gmail.com>
2025-04-26 09:59:10 -0700

    1) Adding object oriented code. 2) This way we can make a gameboard object and no need to have one global variable.

--- src/GameLogic.h
+++ src/GameLogic.h
@@ -5,14 +5,5 @@
 #include <vector>
 using namespace std;
 
-// Declare functions only
-void initializeBoard();
-vector<vector<char>> getBoard();
-bool isValidRow(int row, char target) ;
-bool isValidCol(int col, char target);
-bool isValidDiagonal(char target);
-bool hasWon();
-bool isDraw();
-bool playerMakeMove(int row, int col, char playerChoice);
 
 #endif
