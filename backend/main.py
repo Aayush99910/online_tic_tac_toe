fcdd5b317abf76ddaaa9fbc3f1dcc80bd59c8483
Aayush <amaharjann01@gmail.com>
2025-05-08 15:08:26 -0700

    1) user sends user_id and we pass it as argument to make_move function 2) make_move function is changed and now uses room_manager to handle moves not each room has its own board. 3) build_response function is added that builds a response based on arguments provided.

--- backend/main.py
+++ backend/main.py
@@ -18,65 +18,69 @@ app.add_middleware(
 # global room manager object
 app.room_manager = RoomManager()
 
+# builing a response to avoid repitition
+def build_response(message: str, room: object, win: bool, draw: bool, valid: bool) -> dict:
+    return {
+        "symbol": room["symbol"],
+        "status": 0,
+        "valid": valid,
+        "message": message,
+        "win": win,
+        "draw": draw,
+        "board": room["board"].getBoard(),
+        "current_player": room["current_player"]
+    }
+
+
 # make move function 
-async def make_move(row: int, col: int, websocket: WebSocket):
-    print(row, col)
-    # # getting that room id 
-    # room_id = app.room_manager.websocket_to_room_id[websocket]
-    # if not room_id:
-    #     await websocket.send_json({"valid": False, "message": "Room not found."})
-    #     return
+async def make_move(row: int, col: int, user_id: str, websocket: WebSocket):
+    # getting that room id 
+    room_id = app.room_manager.get_room_id_with_websocket(websocket)
+    if not room_id:
+        await websocket.send_json({"valid": False, "message": "Room not found."})
+        return
     
-    # # getting that room
-    # room = app.room_manager.get_room_with_id(room_id)
-    # if not room:
-    #     await websocket.send_json({"valid": False, "message": "Invalid room."})
-    #     return
+    # getting that room
+    room = app.room_manager.get_room_with_id(room_id)
+    if not room:
+        await websocket.send_json({"valid": False, "message": "Invalid room."})
+        return
     
-    # current_player = room["current_player"]
+    # current player 
+    current_player = room["current_player"]
+    symbol = room["symbol"]
+    symbol_to_move = symbol[user_id]
 
-#     if gamelogic.playerMakeMove(row, col, current_player):
-#         if gamelogic.hasWon():
-#             response1 = {
-#                 "message": f"Player {current_player} wins!",
-#                 "valid": True,
-#                 "win": True,
-#                 "draw": False, 
-#                 "board": gamelogic.getBoard()
-#             }
+    if current_player == symbol_to_move:
+        # get the gameboard
+        board = room["board"]
 
-#         elif gamelogic.isDraw():
-#             response1 = {
-#                 "message": "It a tie!",
-#                 "valid": True,
-#                 "win": False,
-#                 "draw": True, 
-#                 "board": gamelogic.getBoard()
-#             }
-#         else:
-#             room["current_player"] = 'O' if current_player == 'X' else 'X'
-#             response1 = {
-#                 "message": f"Player {current_player} made a move!",
-#                 "valid": True,
-#                 "win": False,
-#                 "draw": False, 
-#                 "board": gamelogic.getBoard(),
-#                 "current_player": room["current_player"]
-#             }
+        # making a move
+        if board.playerMakeMove(row, col, symbol_to_move):
+            # need to change the current_player to another player 
+            room["current_player"] = 'O' if room["current_player"] == 'X' else 'X'
 
-#         # Broadcast to all players in the room
-#         for player in room["players_array"]:
-#             await player.send_json(response1)
-#     else:
-#         response1 = {
-#             "message": "Invalid Move! Please try again!",
-#             "valid": False,
-#             "win": False,
-#             "draw": False, 
-#             "board": gamelogic.getBoard(),
-#             "current_player": room["current_player"]
-#         }
-#         await websocket.send_json(response1)
+            # three condition 
+            # win
+            if board.hasWon():
+                res = build_response(f"Player {symbol_to_move} won!", room, True, False, True) 
+            # draw
+            elif board.isDraw():
+                res =  build_response("It is a tie game!", room, False, True, True)
+            else:
+                # normal move
+                res =  build_response(f"Player {symbol_to_move} made a move!", room, False, False, True)
+                
+            # boardcast to all players in that room
+            for websocket_object in room["websocket_objects"]:
+                await websocket_object.send_json(res)
+        else:
+            res = build_response("Invalid move. Please try again!", room, False, False, False)
+            await websocket.send_json(res)
+    else:
+        res = build_response("It's not your turn.", room, False, False, False)
+        await websocket.send_json(res)
+
 
 # Home page for welcoming the users
 @app.get("/")
@@ -126,7 +130,8 @@ async def websocket_endpoint(player_id: str, player_websocket: WebSocket):
                 data = json.loads(await player_websocket.receive_text())
                 row = int(data['row'])
                 col = int(data['col'])
-                await make_move(row, col, player_websocket)
+                user_id = str(data['user_id'])
+                await make_move(row, col, user_id, player_websocket)
         except WebSocketDisconnect:
             pass 
             # app.room_manager.remove_from_queue(player_id)
