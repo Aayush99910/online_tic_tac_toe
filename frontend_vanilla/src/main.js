85d384a480f1da81a1e5273247fe7e05a3af54d1
Aayush <amaharjann01@gmail.com>
2025-05-14 12:07:44 -0700

    feat: started UI state management and reorganized DOM controls (WIP)

--- frontend_vanilla/src/main.js
+++ frontend_vanilla/src/main.js
@@ -1,14 +1,22 @@
+/*
+CONSTRAINTS AND VARIABLES
+*/
 const boardContainer = document.getElementById("game-board");
+
 const statusElement = document.getElementById("status");
 const playRandomBtn = document.getElementById("play-random-btn");
 const hostRoomBtn = document.getElementById("host-room-btn");
+
 const watchBtn = document.getElementById("watch-btn");
-const privateControls = document.getElementById("private-controls");
-const createRoomBtn = document.getElementById("create-room-btn");
-const joinRoomToPlayBtn = document.getElementById("join-room-to-play-btn");
 const roomInputContainer = document.getElementById("room-input-container");
 const roomInput = document.getElementById("room-id-input");
 const submitRoomBtn = document.getElementById("submit-room-id");
+
+const privateControls = document.getElementById("private-controls");
+const createRoomBtn = document.getElementById("create-room-btn");
+const joinRoomToPlayBtn = document.getElementById("join-room-to-play-btn");
+const goBackFromHostRoomBtn = document.getElementById("private-back-btn");
+
 const controlButtons= document.getElementById("control-buttons");
 const quitBtn = document.getElementById("quit-game-btn");
 const resetBtn = document.getElementById("reset-game-btn");
@@ -16,6 +24,74 @@ const leaveBtn = document.getElementById("leave-game-btn");
 
 let ws;
 let userId;
+
+/*
+UTILITY FUNCTIONS
+*/
+function setUIState(state) {
+    // getting all the elements and adding them to an array 
+    const allElements = [playRandomBtn, hostRoomBtn, privateControls, createRoomBtn, joinRoomToPlayBtn, goBackFromHostRoomBtn, watchBtn, roomInputContainer, roomInput, submitRoomBtn, controlButtons, quitBtn, resetBtn, leaveBtn];
+
+    // need to hide every elements first
+    allElements.forEach(element => element.style.display = 'none');
+
+    // now having various states using switch 
+    switch(state){
+        case 'waiting':
+            // everything needs to be none so no changes just doing this 
+            playRandomBtn.style.display = 'none';
+            break 
+        case 'game started':
+            quitBtn.style.display = 'inline-block';
+    }
+}
+
+// sending the players move and their userId to the backend
+function sendMove(i, j) {
+    const move = { 
+        row: i, 
+        col: j,
+        user_id: userId
+    };  
+    ws.send(JSON.stringify(move));
+}
+
+/*
+    render board function that takes in two argument first is the board itself 
+    that we are going to render. board argument is an array that will be turned 
+    into html elements for user to interact with 
+    isTurn argument is taken because we want to have the click event listener 
+    only for player whose turn is happening right now else we don't have to have 
+    this event listener for that player
+*/
+function renderBoard(board, isTurn) {
+    boardContainer.innerHTML = "";
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            const div = document.createElement("div");
+            div.className = "cell";
+            div.textContent = board[i][j];
+            
+            // Add color styling based on cell content
+            if (div.textContent === 'X') {
+                div.style.color = '#E64833';
+            } else if (div.textContent === 'O') {
+                div.style.color = '#90AEAD';
+            }
+
+            // adding the event listener only if it is their turn
+            if (isTurn) {
+                div.addEventListener("click", () => sendMove(i, j));
+            }
+
+            // adding this to the parent div
+            boardContainer.appendChild(div);
+        }
+    }
+}
+
+
+
 
 /* 
   When the page loads we want to call the the '/' endpoint from the backend
@@ -39,19 +115,10 @@ document.addEventListener("DOMContentLoaded", async () => {
 })
 
 
-function sendMove(i, j) {
-    const move = { 
-        row: i, 
-        col: j,
-        user_id: userId
-    };  
-    ws.send(JSON.stringify(move));
-}
-
 function handleServerMessageForPlayers(data, userId) {
     if (data.status === 1) {
         statusElement.innerText = data.message;
-        controlButtons.style.display = 'none'; 
+        setUIState("waiting");
         return
     }
 
@@ -63,8 +130,10 @@ function handleServerMessageForPlayers(data, userId) {
         
         // return this player back to home
         statusElement.innerText = data.message;
+        console.log("THIS IS WHERE I AM FOR BOTH PLAYERS")
         controlButtons.style.display = 'none';
-        startBtn.style.display = 'inline-block';
+        playRandomBtn.style.display = 'inline-block';
+        hostRoomBtn.style.display = 'inline-block';
         watchBtn.style.display = 'inline-block';
         boardContainer.innerHTML = '';
         return 
@@ -106,40 +175,6 @@ function handleServerMessageForSpectators(data) {
 
 }
 
-/*
-    render board function that takes in two argument first is the board itself 
-    that we are going to render board variable is an array that will be turned 
-    into html elements for user to interact with 
-    hasWon argument is taken because we want to have the click event listener 
-    only if the game has not being won if someone wins then we don't have to have 
-    this event listener
-*/
-function renderBoard(board, isTurn) {
-    boardContainer.innerHTML = "";
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-            const div = document.createElement("div");
-            div.className = "cell";
-            div.textContent = board[i][j];
-            
-            // Add color styling based on cell content
-            if (div.textContent === 'X') {
-                div.style.color = '#E64833';
-            } else if (div.textContent === 'O') {
-                div.style.color = '#90AEAD';
-            }
-
-            // adding the event listener only if it is their turn
-            if (isTurn) {
-                div.addEventListener("click", () => sendMove(i, j));
-            }
-
-            // adding this to the parent div
-            boardContainer.appendChild(div);
-        }
-    }
-}
-
 
 /*
     event listener for start button
@@ -154,11 +189,7 @@ playRandomBtn.addEventListener("click", async () => {
         
         // handling the wsopen
         ws.onopen = () => {
-            playRandomBtn.style.display = 'none';
-            hostRoomBtn.style.display = "none";
-            watchBtn.style.display = "none";
-            privateControls.style.display = "none";
-            roomInputContainer.style.display = "none";
+            setUIState('waiting');
         }
 
         // handling in coming messages
@@ -185,6 +216,7 @@ playRandomBtn.addEventListener("click", async () => {
     can join the room and watch other two player play.
 */
 watchBtn.addEventListener("click", () => {
+    statusElement.innerText = "Enter the room id that you want to watch below.";
     roomInputContainer.style.display = "flex";
     playRandomBtn.style.display = "none";
     hostRoomBtn.style.display = "none";
@@ -249,4 +281,17 @@ quitBtn.addEventListener('click', async () => {
     catch(err){
         console.log("Error", err);
     }
+})
+
+/* 
+    when users click on private game we need to show them the create room and 
+    join room buttons and then I will handle that in the backend. Just need 
+    the frontend to work 
+*/
+hostRoomBtn.addEventListener("click", () => {
+    // removing all the other buttons
+    
+    privateControls.style.display = "flex";
+    createRoomBtn.style.display = "inline-block";
+    joinRoomToPlayBtn.style.display = "inline-block";
 })
